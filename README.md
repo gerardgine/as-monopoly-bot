2a41a459bbb6ace040d4a96d85700cb60f882fcb
Gerard Giné <ggine@nvidia.com>
2021-02-14 00:34:15 -0800

    README & some scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # fucking-monopoly-bot
 Simple Telegram bot for Monopoly games
+
+Create virtualenv:
+```shell
+python3 -m venv venv
+```
+
+Activate virtualenv:
+```shell
+source venv/bin/activate
+```
+
+Upgrade pip:
+```shell
+pip install -U pip
+```
+
+Install requirements:
+```shell
+pip install -r requirements.txt
+```
+
+Start local development server:
+```shell
+FLASK_APP=flask_app.py FLASK_ENV=development flask run
+```
--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,25 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
 
+@app.route("/")
+def hello_world():
+    return "Hello from Flask!"
+
+
+@app.route("/start")
+def start_cmd():
+    return "Start"
+
+
+@app.route("/help")
+def help_cmd():
+    return "Help"
+
+
+@app.route("/settings")
+def settings_cmd():
+    return "Settings"
