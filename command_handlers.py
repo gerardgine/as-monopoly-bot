8bff43ed9977e1f4d7fdcb43fc0e9f84711dea4d
Gerard Giné <ggine@nvidia.com>
2021-03-10 09:52:21 -0800

    Send multiple polls if there are >10 options

--- command_handlers.py
+++ command_handlers.py
@@ -1,8 +1,11 @@
+import math
 import random
 
 import telegram
 
 from responses import success
+
+MAX_POLL_OPTIONS = 10
 
 
 def get_chat_id_from_update(update: telegram.Update):
@@ -27,22 +30,48 @@ def settings_cmd(update: telegram.Update, bot):
     return success(text)
 
 
+def send_poll_with_split_options(update: telegram.Update, bot, poll_options, question):
+    """
+    Having to do this sucks, big time, but Telegram limits the amount of options per
+    poll to 10 (as of March 10th, 2021), so if our poll has too many options, we'll
+    send multiple polls and hope each user doesn't vote in more than one of the polls...
+
+    ¯\_(ツ)_/¯
+    """
+    options_count = len(poll_options)
+    options_per_poll = (
+        MAX_POLL_OPTIONS
+        if options_count % MAX_POLL_OPTIONS != 1
+        else MAX_POLL_OPTIONS - 1
+    )
+    current_batch = 1
+    total_batches = math.ceil(options_count / options_per_poll)
+    for i in range(0, options_count, options_per_poll):
+        if total_batches == 1:
+            updated_question = question
+        else:
+            updated_question = f"{question} ({current_batch}/{total_batches})"
+        bot.send_poll(
+            get_chat_id_from_update(update),
+            question=updated_question,
+            options=poll_options[i : min(options_count, i + options_per_poll)],
+            is_anonymous=False,
+        )
+        current_batch += 1
+
+
 def board_poll_cmd(update: telegram.Update, bot):
     text = "Board poll"
-    bot.send_poll(
-        get_chat_id_from_update(update),
-        question="Quin taulell voleu fer servir?",
-        options=[
-            "Clàssic",
-            "Monstropolis",
-            "Paris, La Belle Epoque",
-            "1935 Atlantic City",
-            "Snowdrop Valley",
-            "Victorian London",
-            "Cherry Blossom Tokyo",
-        ],
-        is_anonymous=False,
-    )
+    boards = [
+        "Clàssic",
+        "Monstropolis",
+        "Paris, La Belle Epoque",
+        "1935 Atlantic City",
+        "Snowdrop Valley",
+        "Victorian London",
+        "Cherry Blossom Tokyo",
+    ]
+    send_poll_with_split_options(update, bot, boards, "Quin taulell voleu fer servir?")
     return success(text)
 
 
@@ -65,62 +94,57 @@ def get_shitty_offer_options():
 
 def shitty_offer_poll_cmd(update: telegram.Update, bot):
     text = "Mierdaoferta poll"
-    bot.send_poll(
-        get_chat_id_from_update(update),
-        question="Mierdaoferta?",
-        options=get_shitty_offer_options(),
-        is_anonymous=False,
+    send_poll_with_split_options(
+        update, bot, get_shitty_offer_options(), "Mierdaoferta?"
     )
     return success(text)
 
 
 def city_poll_cmd(update: telegram.Update, bot):
     text = "City poll"
-    bot.send_poll(
-        get_chat_id_from_update(update),
-        question="Quina ciutat voleu fer servir amb el taulell clàssic?",
-        options=[
-            "Àustria - Austria",
-            "Bòsnia - Bosnia",
-            "Brasil - Brazil",
-            "Canadà (FR) - Canada (FR)",
-            "Croàcia - Croatia",
-            "República Txeca - Czechia",
-            "Dinamarca - Denmark",
-            "Estònia - Estonia",
-            "Finlàndia - Finland",
-            "França - France",
-            "Alemanya - Germany",
-            "Hong Kong - Hong Kong",
-            "Hongria - Hungary",
-            "Índia - India",
-            "Indonèsia - Indonesia",
-            "Irlanda - Ireland",
-            "Itàlia - Italy",
-            "Amèrica Llatina - Latin America",
-            "Letònia - Latvia",
-            "Lituània - Lithuania",
-            "Malàisia - Malaysia",
-            "Mèxic - Mexico",
-            "Països Baixos - Netherlands",
-            "Noruega - Norway",
-            "Polònia - Poland",
-            "Portugal - Portugal",
-            "Romania - Romania",
-            "Rússia - Russia",
-            "Sèrbia - Serbia",
-            "Eslovàquia - Slovakia",
-            "Eslovènia - Slovenia",
-            "Sud-Àfrica - South Africa",
-            "Espanya - Spain",
-            "Espanya (català) - Spain (Catalan)",
-            "Suècia - Sweden",
-            "Suïssa (FR) - Switzerland (FR)",
-            "Suïssa (DE) - Switzerland (DE)",
-            "Turquia - Turkey",
-            "Regne Unit - UK",
-            "EUA - USA",
-        ],
-        is_anonymous=False,
+    cities = [
+        "Àustria - Austria",
+        "Bòsnia - Bosnia",
+        "Brasil - Brazil",
+        "Canadà (FR) - Canada (FR)",
+        "Croàcia - Croatia",
+        "República Txeca - Czechia",
+        "Dinamarca - Denmark",
+        "Estònia - Estonia",
+        "Finlàndia - Finland",
+        "França - France",
+        "Alemanya - Germany",
+        "Hong Kong - Hong Kong",
+        "Hongria - Hungary",
+        "Índia - India",
+        "Indonèsia - Indonesia",
+        "Irlanda - Ireland",
+        "Itàlia - Italy",
+        "Amèrica Llatina - Latin America",
+        "Letònia - Latvia",
+        "Lituània - Lithuania",
+        "Malàisia - Malaysia",
+        "Mèxic - Mexico",
+        "Països Baixos - Netherlands",
+        "Noruega - Norway",
+        "Polònia - Poland",
+        "Portugal - Portugal",
+        "Romania - Romania",
+        "Rússia - Russia",
+        "Sèrbia - Serbia",
+        "Eslovàquia - Slovakia",
+        "Eslovènia - Slovenia",
+        "Sud-Àfrica - South Africa",
+        "Espanya - Spain",
+        "Espanya (català) - Spain (Catalan)",
+        "Suècia - Sweden",
+        "Suïssa (FR) - Switzerland (FR)",
+        "Suïssa (DE) - Switzerland (DE)",
+        "Turquia - Turkey",
+        "Regne Unit - UK",
+        "EUA - USA",
+    ]
+    send_poll_with_split_options(
+        update, bot, cities, "Quina ciutat voleu fer servir amb el taulell clàssic?"
     )
     return success(text)
